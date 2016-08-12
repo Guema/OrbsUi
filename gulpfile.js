6193a57fe0fc814db7612ec78ec09cd22965b98d
Guema <floorent1993@gmail.com>
2016-08-13 01:56:25 +0200

    Added a watcher system and enhanced build task

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,21 @@ var clean = require('gulp-clean');
 var AddonName = 'OrbsUi';
 var WowPATH = 'D:/World of Warcraft/';
 var toInclude = [
-  './*.xml',
-  './*.lua',
-  './*.toc',
+  './**/*.xml',
+  './**/*.lua',
+  './**/*.toc',
   'LICENSE',
-  'README.md'
+  'README.md',
 ];
 var toExclude = [
 
 ];
+
+var watcher = gulp.watch(toInclude, ['default']);
+
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
 
 gulp.task('build', function() {
   return gulp.src(toInclude).pipe(gulp.dest(WowPATH + '/Interface/Addons/' + AddonName));
@@ -24,4 +30,4 @@ gulp.task('clean', function() {
   return gulp.src(WowPATH + '/Interface/Addons/' + AddonName + "/*", {read: false}).pipe(clean());
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['clean','build']);
